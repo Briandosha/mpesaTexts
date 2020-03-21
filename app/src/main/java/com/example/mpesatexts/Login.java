51272f8fe5a64fea2fcb3d88ce7ce936fe8e7d5d
Your Name <you@example.com>
2020-03-22 02:14:29 +0300

    doshi steel

--- app/src/main/java/com/example/mpesatexts/Login.java
+++ app/src/main/java/com/example/mpesatexts/Login.java
@@ -2,13 +2,100 @@ package com.example.mpesatexts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
+
+    public TextInputEditText emailTx, passwordTx;
+    public MaterialButton btnLogin;
+    public String email, password;
+    LinearLayout linearLayoutSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailTx =  findViewById(R.id.ed_email);
+        passwordTx =  findViewById(R.id.ed_pass);
+        linearLayoutSignup =  findViewById(R.id.layoutSignup);
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailTx.getText().toString().trim();
+                password = passwordTx.getText().toString().trim();
+
+                if (validateLogin()){
+                    Intent intent =  new Intent(Login.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+
+
+                else {
+                    Toast.makeText(Login.this, "correct all the fields and try again", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        linearLayoutSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(Login.this,SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
+
     }
+
+
+    public boolean validateLogin(){
+        boolean valid = true;
+        if (email == null){
+            valid = false;
+            emailTx.setError("email cannot be null");
+
+        }
+
+        if(password == null){
+            valid = false;
+            passwordTx.setError("password cannot be null");
+
+        }
+
+        if(email!= null && email.equals("")){
+            valid = false;
+            emailTx.setError("email cannot be empty");
+
+
+        }
+
+        if(password!= null && password.equals("")){
+            valid = false;
+            passwordTx.setError("password cannot be empty");
+
+
+        }
+
+        return valid;
+    }
+
 }
--- app/src/main/java/com/example/mpesatexts/SignUp.java
+++ app/src/main/java/com/example/mpesatexts/SignUp.java
@@ -2,13 +2,113 @@ package com.example.mpesatexts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUp extends AppCompatActivity {
+    public TextInputEditText emailTx, passwordTx, confirmTx;
+    public MaterialButton btnSignup;
+    public String email, password, confirmPassword;
+    LinearLayout linearLayoutLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        emailTx =  findViewById(R.id.ed_email);
+        passwordTx =  findViewById(R.id.ed_pass);
+        confirmTx =  findViewById(R.id.ed_confirmpass);
+        linearLayoutLogin =  findViewById(R.id.layoutSignup);
+        btnSignup = findViewById(R.id.btn_signup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailTx.getText().toString().trim();
+                password = passwordTx.getText().toString().trim();
+
+                if (validateLogin()){
+                    Intent intent =  new Intent(SignUp.this,Login.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+
+
+                else {
+                    Toast.makeText(SignUp.this, "correct all the fields and try again", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        linearLayoutLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(SignUp.this,SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
     }
+
+
+    public boolean validateLogin(){
+        boolean valid = true;
+        if (email == null){
+            valid = false;
+            emailTx.setError("email cannot be null");
+
+        }
+
+        if(password == null){
+            valid = false;
+            passwordTx.setError("password cannot be null");
+
+        }
+
+        if(email!= null && email.equals("")){
+            valid = false;
+            emailTx.setError("email cannot be empty");
+
+
+        }
+
+        if(password!= null && password.equals("")){
+            valid = false;
+            passwordTx.setError("password cannot be empty");
+
+
+        }
+
+
+        if(confirmPassword == null){
+            valid = false;
+            confirmTx.setError("confirm password cannot be null");
+
+        }
+
+
+        if(confirmPassword!= null && confirmPassword.equals("")){
+            valid = false;
+            confirmTx.setError("confirm password cannot be empty");
+
+
+        }
+
+        return valid;
+    }
+
 }
